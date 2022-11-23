412c3885bdeada09e1a05cd8aa739fcf84a99bbb
Anasemos Kassahun <anasemosaka@gmail.com>
2022-11-22 19:54:56 -0500

    added comments

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -7,14 +7,19 @@ import NoImg from "./no-image.png";
 import TrailerMovies from "../Trailers/TrailerMovies";
 
 function Movies() {
+  // dark mode / light mode
   const { toggle, inputValue } = useContext(Container);
   const input = inputValue;
+  // all movie being pulled and set 
   const [moviesData, setMoviesData] = useState([]);
   const [trailer, setTrailer] = useState(true);
   const [movieTitle, setMovieTitle] = useState("");
   const Shown = input ? "search" : "discover";
   const Api = `https://api.themoviedb.org/3/${Shown}/movie`;
   const Images = "https://image.tmdb.org/t/p/w500/";
+
+  // requests Api
+  // takes 2 params  
   const MovieCall = async () => {
     const data = await axios.get(Api, {
       params: {
@@ -22,15 +27,20 @@ function Movies() {
         query: input,
       },
     });
+    // gets data and set the results to MoviesData 
     const results = data.data.results;
     setMoviesData(results);
   };
+
+  // When movie function is called setTimeout sets a timer which executes code 
+  // In this case when searching for a movie you get it instantly
   useEffect(() => {
     setTimeout(() => {
       MovieCall();
     }, 50);
   }, [input]);
 
+  
   const MoviesTitle = (movie) => {
     setMovieTitle(movie.title);
     setTrailer(!trailer);
